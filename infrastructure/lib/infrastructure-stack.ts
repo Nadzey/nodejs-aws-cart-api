b7076b72bed2ca00188a33758a01fe5e964d9998
Nadzey <nadiakoluzaeva@gmail.com>
2025-03-28 10:09:43 -0400

    Task-8 work

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -2,21 +2,23 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as path from 'path';
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const cartLambda = new lambda.Function(this, 'CartLambda', {
+    const lambdaFn = new lambda.Function(this, 'CartServiceLambda', {
       runtime: lambda.Runtime.NODEJS_18_X,
       handler: 'main.handler',
-      code: lambda.Code.fromAsset('../dist'),
-      memorySize: 512,
-      timeout: cdk.Duration.seconds(10),
+      code: lambda.Code.fromAsset(path.join(__dirname, '../../dist')),
+      environment: {
+        NODE_ENV: 'production',
+      },
     });
 
-    new apigateway.LambdaRestApi(this, 'CartApi', {
-      handler: cartLambda,
+    new apigateway.LambdaRestApi(this, 'CartServiceAPI', {
+      handler: lambdaFn,
       proxy: true,
     });
   }
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,25 @@
 import { NestFactory } from '@nestjs/core';
-
-import helmet from 'helmet';
-
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
+import { Handler, Context } from 'aws-lambda';
+import { CartService } from './cart/services/cart.service';
+
+let appContext;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  const configService = app.get(ConfigService);
-
-  const port = configService.get('APP_PORT') || 4000;
-
-  app.enableCors({
-    origin: (req, callback) => callback(null, true),
-  });
-  app.use(helmet());
-
-  await app.listen(port, () => {
-    console.log('App is running on %s port', port);
-  });
+  if (!appContext) {
+    appContext = await NestFactory.createApplicationContext(AppModule);
+  }
+  return appContext;
 }
-bootstrap();
+
+export const handler: Handler = async (event: any, context: Context) => {
+  const app = await bootstrap();
+  const cartService = app.get(CartService);
+  const userId = event.queryStringParameters?.userId || 'guest';
+  const cart = cartService.findOrCreateByUserId(userId);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(cart),
+  };
+};
